f3de35e8e4c51631a66dd61849de7b63b7904fd8
Rarrix <rarrix1@gmail.com>
2018-11-06 02:55:56 +0200

    yeayea

---
+++ sad/Music Videos/Assets/Music Videos/Mana.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Mana : MonoBehaviour
+{
+
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+           SceneManager.LoadScene(0);
+        }
+    }
+}
+
+
